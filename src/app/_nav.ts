e8ce089a007ab6dcde1da9a92bded03516d1809e
ilovelili <route666@live.cn>
2019-04-29 15:41:34 +0800

    comment out dead link temporarily

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -14,24 +14,24 @@ export const navigation = [
     url: '/教师信息',
     icon: 'icon-user-female'
   },
-  {
-    name: '成长档案',
-    url: '/theme/typography',
-    icon: 'icon-note'
-  },
+  // {
+  //   name: '成长档案',
+  //   url: '/theme/typography',
+  //   icon: 'icon-note'
+  // },
   {
     name: '出勤信息',
     url: '/出勤信息',
     icon: 'icon-user'
   },
-  {
-    name: '体格检查',
-    url: '/theme/typography',
-    icon: 'icon-star'
-  },
-  {
-    name: '膳食管理',
-    url: '/theme/typography',
-    icon: 'icon-cup'
-  },
+  // {
+  //   name: '体格检查',
+  //   url: '/theme/typography',
+  //   icon: 'icon-star'
+  // },
+  // {
+  //   name: '膳食管理',
+  //   url: '/theme/typography',
+  //   icon: 'icon-cup'
+  // },
 ];
