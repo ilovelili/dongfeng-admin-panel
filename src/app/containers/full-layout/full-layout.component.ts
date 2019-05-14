3e3a21d6242b27203edf50423fbcefe40f2f2edd
ilovelili <route666@live.cn>
2019-05-14 22:46:07 +0800

    loading icon on navigation change

--- src/app/containers/full-layout/full-layout.component.ts
+++ src/app/containers/full-layout/full-layout.component.ts
@@ -1,7 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './full-layout.component.html',
+  templateUrl: './full-layout.component.html',  
 })
-export class FullLayoutComponent { }
+export class FullLayoutComponent implements OnInit, AfterViewChecked {    
+  private loading: boolean;
+  private seed: number;
+
+  constructor(private router: Router) {
+    this.loading = true;
+  }
+
+  ngOnInit(): void {
+    this.router.events.filter(event => event instanceof NavigationStart).subscribe(e => {      
+      this.loading = true;
+      if (!this.seed) {
+        this.seed = window.setInterval(() => this.checkloading(), 100);
+      }
+    });
+  }  
+
+  ngAfterViewChecked(): void {
+    if (!this.seed) {
+      this.seed = window.setInterval(() => this.checkloading(), 100);
+    }
+  }
+
+  checkloading() {
+    let body = document.getElementsByClassName('card');
+    if (body.length > 0 && body[0].hasAttribute('hidden') == false) {
+      this.loading = false;
+      window.clearInterval(this.seed);
+      this.seed = 0;
+    }
+  }
+}
