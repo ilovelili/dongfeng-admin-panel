22c7d5224242119be56e5aeed4d41777b0790c00
ilovelili <route666@live.cn>
2019-05-15 10:48:30 +0800

    clear seed to deregister interval

--- src/app/containers/full-layout/full-layout.component.ts
+++ src/app/containers/full-layout/full-layout.component.ts
@@ -3,37 +3,45 @@ import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './full-layout.component.html',  
+  templateUrl: './full-layout.component.html',
 })
-export class FullLayoutComponent implements OnInit, AfterViewChecked {    
+export class FullLayoutComponent implements OnInit, AfterViewChecked {
   private loading: boolean;
-  private seed: number;
+  private seed1: number;
+  private seed2: number;
 
   constructor(private router: Router) {
     this.loading = true;
   }
 
   ngOnInit(): void {
-    this.router.events.filter(event => event instanceof NavigationStart).subscribe(e => {      
+    this.router.events.filter(event => event instanceof NavigationStart).subscribe(e => {
       this.loading = true;
-      if (!this.seed) {
-        this.seed = window.setInterval(() => this.checkloading(), 100);
+      if (!this.seed1) {
+        this.seed1 = window.setInterval(() => this.checkloading(this.seed1, ()=>this.clearseed1(this)), 100);
       }
     });
-  }  
+  }
 
   ngAfterViewChecked(): void {
-    if (!this.seed) {
-      this.seed = window.setInterval(() => this.checkloading(), 100);
+    if (!this.seed2) {
+      this.seed2 = window.setInterval(() => this.checkloading(this.seed2), 100);
     }
   }
 
-  checkloading() {
+  checkloading(seed: number, callback?: () => void) {
     let body = document.getElementsByClassName('card');
     if (body.length > 0 && body[0].hasAttribute('hidden') == false) {
       this.loading = false;
-      window.clearInterval(this.seed);
-      this.seed = 0;
+      window.clearInterval(seed);
+
+      if (callback) {
+        callback();
+      }
     }
+  }
+
+  clearseed1(me: any) {
+    me.seed1 = 0;
   }
 }
