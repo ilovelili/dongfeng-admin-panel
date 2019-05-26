e91acd5a6e2cfa4a398feed89d877361de2f4643
ilovelili <route666@live.cn>
2019-05-26 21:55:46 +0800

    browser detection

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,34 @@ if (environment.production) {
   enableProdMode();
 }
 
-// check if IE
-let ua = navigator.userAgent;
-  /* MSIE used to detect old browsers and Trident used to newer ones*/
-if (ua.indexOf("MSIE ") > -1) {
-  // legacy IE
-  window.alert('您的浏览器不支持该系统，推荐使用谷歌或者火狐浏览器。')
-} else if (ua.indexOf("Trident/") > -1) {
-  // new IE version. But IE is IE
-  window.alert('您的浏览器有可能不支持该系统，推荐使用谷歌或者火狐浏览器。')
-}  
+declare var window, opr, InstallTrigger, document, safari: any;
+// Opera 8.0+
+const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+// Firefox 1.0+
+const isFirefox = typeof InstallTrigger !== 'undefined';
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+// Internet Explorer 6-11
+const isIE = /*@cc_on!@*/false || !!document.documentMode;
+// Edge 20+
+const isEdge = !isIE && !!window.StyleMedia;
+// Chrome 1 - 71
+const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+// Blink engine detection
+const isBlink = (isChrome || isOpera) && !!window.CSS;
+
+if (isChrome || isFirefox || isBlink) {
+  // good
+} else if (isOpera) {
+  window.alert('Opera浏览器有可能不支持该系统，您肯呢个会看到空白页面。推荐使用谷歌或者火狐浏览器。')
+} else if (isSafari) {
+  window.alert('Safati浏览器有可能不支持该系统，您肯呢个会看到空白页面。推荐使用谷歌或者火狐浏览器。')
+} else if (isIE) {
+  window.alert('IE浏览器有可能不支持该系统，您肯呢个会看到空白页面。推荐使用谷歌或者火狐浏览器。')
+} else if (isEdge) {
+  window.alert('Edge浏览器有可能不支持该系统，您肯呢个会看到空白页面。推荐使用谷歌或者火狐浏览器。')
+} else {
+  window.alert('您的浏览器有可能不支持该系统，您肯呢个会看到空白页面。推荐使用谷歌或者火狐浏览器。')
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
