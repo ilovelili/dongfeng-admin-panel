6622111752c055c6a4a02f4f17248061414e9a11
ilovelili <route666@live.cn>
2019-06-13 21:34:55 +0800

    fix deploy batch

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "build"
 npm run build -- --output-path=./dist/out --environment prod
 
 echo "scp"
-scp -r ./dist/out root@47.110.143.96:/var/www/dongfeng
+scp -r ./dist/out/* root@47.110.143.96:/var/www/dongfeng
 
 echo "done"
