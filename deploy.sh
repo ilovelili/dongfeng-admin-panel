dd4f4c65797f3d1eef98c6ff9da0681280eee9ff
ilovelili <route666@live.cn>
2019-06-13 23:10:07 +0800

    scp assets

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,11 @@ set -e
 echo "build"
 npm run build -- --output-path=./dist/out --environment prod
 
-echo "scp"
+echo "scp compiled files"
 scp -r ./dist/out/* root@47.110.143.96:/var/www/dongfeng
+
+echo "scp assets"
+scp -r ./src/assets/img/* root@47.110.143.96:/root/dongfeng/img
+scp -r ./src/assets/img/* root@47.110.143.96:/var/www/html/img
 
 echo "done"
