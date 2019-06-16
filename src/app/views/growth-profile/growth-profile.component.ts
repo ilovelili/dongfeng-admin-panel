5f061d44b78645f9dd6c11311e5e91290f0b2892
ilovelili <route666@live.cn>
2019-06-16 12:00:19 +0800

    move browser check to growth profile

--- src/app/views/growth-profile/growth-profile.component.ts
+++ src/app/views/growth-profile/growth-profile.component.ts
@@ -7,7 +7,7 @@ import { ToasterService } from 'angular2-toaster';
 import { environment } from 'environments/environment';
 import { Profiles, FormattedProfile, Pupils, Pupil } from 'app/models';
 
-declare var grapesjs: any;
+declare var grapesjs, window, opr, InstallTrigger, document, safari: any;
 
 @Component({
   templateUrl: './growth-profile.component.html',
@@ -37,6 +37,7 @@ export class GrowthProfileComponent extends ViewComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.browsercheck();
     this.getprofiles();
   }
 
@@ -50,7 +51,7 @@ export class GrowthProfileComponent extends ViewComponent implements OnInit {
           }
           if (this.currentClass) {
             this.classes.push(this.currentClass);
-          }          
+          }
 
           this.loading = false;
           this.profiles = new Profiles(d.profiles).format();
@@ -283,7 +284,7 @@ export class GrowthProfileComponent extends ViewComponent implements OnInit {
     };
 
     this.profileClient.updateEBook(profile, images, html, css).subscribe(
-      () => {},
+      () => { },
       e => console.error(e),
       () => this.LogComplete('profile component update ebook completed')
     );
@@ -330,16 +331,16 @@ export class GrowthProfileComponent extends ViewComponent implements OnInit {
         headers: this.profileClient.rawHeaders,
         contentTypeJson: true,
         credentials: 'include',
-        onComplete: () => {          
+        onComplete: () => {
           window.setTimeout(() => {
             let body = this.editor.getHtml();
             if (!body) {
               return;
             }
             let images = this.readImgUrls(this.editor),
-                html = `<!doctype html><html lang="en"><head><meta charset="utf-8"><link rel="stylesheet" href="./css/style.css"><title="${this.editor.getConfig().title}"></head><body>${body}</body></html>`.replace(/assets\/img/g, './img'),
-                css = this.editor.getCss().replace(/assets\/img/g, '../img') + this.chromePrintCSS();
-            this.updateEBookContent(images, html, css);            
+              html = `<!doctype html><html lang="en"><head><meta charset="utf-8"><link rel="stylesheet" href="./css/style.css"><title="${this.editor.getConfig().title}"></head><body>${body}</body></html>`.replace(/assets\/img/g, './img'),
+              css = this.editor.getCss().replace(/assets\/img/g, '../img') + this.chromePrintCSS();
+            this.updateEBookContent(images, html, css);
           }, 5000)
         }
       },
@@ -464,10 +465,46 @@ export class GrowthProfileComponent extends ViewComponent implements OnInit {
       cssimages = this.imgCSS(ed);
 
     listaImgs = htmlimages.concat(cssimages);
-    for (let i = 0; i < listaImgs.length; i++) {      
+    for (let i = 0; i < listaImgs.length; i++) {
       content.push(listaImgs[i].replace(/assets\/img/g, './img'))
     };
 
     return content
+  }
+
+  browsercheck() {
+    // Opera 8.0+
+    const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Firefox 1.0+
+    const isFirefox = typeof InstallTrigger !== 'undefined';
+    // Safari 3.0+ "[object HTMLElementConstructor]" 
+    const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+    // Internet Explorer 6-11
+    const isIE = /*@cc_on!@*/false || !!document.documentMode;
+    // Edge 20+
+    const isEdge = !isIE && !!window.StyleMedia;
+    // Chrome 1 - 71
+    const isChrome = !!window.chrome;
+    // Blink engine detection
+    const isBlink = (isChrome || isOpera) && !!window.CSS;
+
+    // do not alert multiple times by setting a flag in cookie
+    if (document.cookie.indexOf("showalert") == -1) {
+      if (isChrome || isFirefox || isBlink) {
+        // good
+      } else if (isOpera) {
+        window.alert('Opera浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
+      } else if (isSafari) {
+        window.alert('Safati浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
+      } else if (isIE) {
+        window.alert('IE浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
+      } else if (isEdge) {
+        window.alert('Edge浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
+      } else {
+        window.alert('您的浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
+      }
+
+      document.cookie = "showalert=true";
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,39 +8,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-declare var window, opr, InstallTrigger, document, safari: any;
-// Opera 8.0+
-const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-// Firefox 1.0+
-const isFirefox = typeof InstallTrigger !== 'undefined';
-// Safari 3.0+ "[object HTMLElementConstructor]" 
-const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-// Internet Explorer 6-11
-const isIE = /*@cc_on!@*/false || !!document.documentMode;
-// Edge 20+
-const isEdge = !isIE && !!window.StyleMedia;
-// Chrome 1 - 71
-const isChrome = !!window.chrome;
-// Blink engine detection
-const isBlink = (isChrome || isOpera) && !!window.CSS;
-
-// do not alert multiple times by setting a flag in cookie
-if (document.cookie.indexOf("showalert") == -1) {
-  if (isChrome || isFirefox || isBlink) {
-    // good
-  } else if (isOpera) {
-    window.alert('Opera浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
-  } else if (isSafari) {
-    window.alert('Safati浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');    
-  } else if (isIE) {
-    window.alert('IE浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
-  } else if (isEdge) {
-    window.alert('Edge浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
-  } else {
-    window.alert('您的浏览器有可能不支持该系统。如果您看到空白页面，请使用谷歌或者火狐浏览器。');
-  }
-  
-  document.cookie = "showalert=true";
-}
-
 platformBrowserDynamic().bootstrapModule(AppModule);
