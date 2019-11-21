3aa3cae065efbb5636049c5aedd38b729792a13c
ilovelili <route666@live.cn>
2019-11-21 19:53:27 +0900

    deploy batch seprated by department

---
+++ deploy_lincang.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "build"
+npm run build -- --output-path=./dist/out --environment prod
+
+echo "scp compiled files"
+scp -r ./dist/out/* root@116.62.187.109:/var/www/dongfeng
+
+echo "scp assets"
+scp -r ./src/assets/img/* root@116.62.187.109:/root/dongfeng/img
+scp -r ./src/assets/img/* root@116.62.187.109:/var/www/html/img
+
+echo "done"
