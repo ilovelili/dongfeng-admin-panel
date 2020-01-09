b1b504ed5c408729af462ad7eea877af998ac941
ilovelili <route666@live.cn>
2020-01-09 15:40:53 +0800

    separate deploy batch

--- deploy_zhonglou.sh
+++ deploy_zhonglou.sh
@@ -6,12 +6,9 @@ npm run build -- --output-path=./dist/out --environment prod
 
 echo "scp compiled files"
 scp -r ./dist/out/* root@47.110.143.96:/var/www/dongfeng
-scp -r ./dist/out/* root@116.62.187.109:/var/www/dongfeng
 
 echo "scp assets"
 scp -r ./src/assets/img/* root@47.110.143.96:/root/dongfeng/img
 scp -r ./src/assets/img/* root@47.110.143.96:/var/www/html/img
-scp -r ./src/assets/img/* root@116.62.187.109:/root/dongfeng/img
-scp -r ./src/assets/img/* root@116.62.187.109:/var/www/html/img
 
 echo "done"
